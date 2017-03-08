df2aa2c8ec76c4504143ec5675b3f948f532087b
Jake McCoubrey <jake.mccoubrey@hotmail.com>
2017-03-08 10:23:39 +0100

    Change DB connection

--- app/Config/database.php
+++ app/Config/database.php
@@ -70,9 +70,9 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'login' => 'root',
+		'password' => 'mccoubrey22',
+		'database' => 'deltion-itt',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
@@ -81,10 +81,9 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'root',
+		'password' => 'mccoubrey22',
+		'database' => 'deltion-itt',
 		'prefix' => '',
-		//'encoding' => 'utf8',
 	);
 }
